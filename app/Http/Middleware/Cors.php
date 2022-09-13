ba21119796d1dce13fe2e64f4f8f61a0b269fdeb
thiagopetherson <thiagopetherson@outlook.com>
2022-09-13 20:13:48 -0300

    Acertando cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,27 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
+        /*
         return $next($request)
         ->header('Access-Control-Allow-Origin', "*")
         ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
         ->header('Access-Control-Allow-Headers', "X-Requested-With, X-Token-Auth, Accept, Authorization, Content-Type");
+        */
+
+        /* @var $response Response */
+        $response = $next($request);
+        if (!$request->isMethod('OPTIONS')) {
+            return $response;
+        }
+        $allow = $response->headers->get('Allow'); // true list of allowed methods
+        if (!$allow) {
+            return $response;
+        }
+        $headers = [
+            'Access-Control-Allow-Methods' => $allow,
+            'Access-Control-Max-Age' => 3600,
+            'Access-Control-Allow-Headers' => 'X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept, Authorization',
+        ];
+        return $response->withHeaders($headers);
     }
 }
